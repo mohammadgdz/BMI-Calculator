b684a2187e180cf3be463782b7d768cc9660f075
mohammadgdz <mhd.gdz@gmail.com>
2023-06-10 23:19:06 +0330

    Initial Commit

---
+++ BMI Calculator/Controllers/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var heightLable: UILabel!
+    
+    @IBOutlet weak var weightLable: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func heightSliderChanged (_ sender: UISlider) {
+        
+        heightLable.text = "\(String(format: "%.2f", sender.value)) m"
+        
+    }
+    
+    @IBAction func weightSliderChanged (_ sender: UISlider) {
+        
+        weightLable.text = "\(Int(sender.value)) Kg"
+
+    }
+}
+
