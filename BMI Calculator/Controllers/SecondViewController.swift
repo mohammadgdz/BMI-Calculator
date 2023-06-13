deca4ef6664f8d36a0385331df7342ffcaf68277
mohammadgdz <mhd.gdz@gmail.com>
2023-06-13 22:25:29 +0330

    add SecondViewController swift file.

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Mohammad on 2023-06-13.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let label = UILabel()
+        label.text = "Hello"
+    }
+}
